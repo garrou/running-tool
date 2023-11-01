21d01c479f1985f6339ce89cb86ce0a73f410f90
garrou <garrousteadrien@gmail.com>
2023-11-01 23:58:32 +0100

    Add compute time with distance and km/h

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 const SECONDS_IN_MIN = 60;
 const SECONDS_IN_HOUR = 360;
+const MINS_IN_HOUR = 60;
 const METER_KM = 1000
 const KM_HOUR = 3600 / METER_KM;
+
+// v = d / t
+// d = v * t
+// t = d / v
 
 document.getElementsByName("btnSk")[0].addEventListener("click", () => {
     const skMeters = document.getElementsByName("skMeters")[0];
@@ -10,12 +15,12 @@ document.getElementsByName("btnSk")[0].addEventListener("click", () => {
     const skSeconds = document.getElementsByName("skSeconds")[0];
     const skResult = document.getElementById("skResult");
 
-    const meter = skMeters.value === "" ? 0 : parseInt(skMeters.value);
+    const meters = skMeters.value === "" ? 0 : parseInt(skMeters.value);
     const hours = skHours.value === "" ? 0 : parseInt(skHours.value);
     const minutes = skMinutes.value === "" ? 0 : parseInt(skMinutes.value);
     const seconds = skSeconds.value === "" ? 0 : parseInt(skSeconds.value);
     const div = hours * SECONDS_IN_HOUR + minutes * SECONDS_IN_MIN + seconds
-    const speed = meter / (isFinite(div) ? div : 1) * KM_HOUR
+    const speed = meters / (isFinite(div) ? div : 1) * KM_HOUR
 
     skResult.textContent = `${speed.toFixed(2)} km/h`;
 });
@@ -26,11 +31,11 @@ document.getElementsByName("btnTk")[0].addEventListener("click", () => {
     const tkSeconds = document.getElementsByName("tkSeconds")[0];
     const tkResult = document.getElementById("tkResult");
 
-    const meter = tkMeters.value === "" ? 0 : parseInt(tkMeters.value);
+    const meters = tkMeters.value === "" ? 0 : parseInt(tkMeters.value);
     const minutes = tkMinutes.value === "" ? 0 : parseInt(tkMinutes.value);
     const seconds = tkSeconds.value === "" ? 0 : parseInt(tkSeconds.value);
 
-    const km = meter / METER_KM;
+    const km = meters / METER_KM;
     const s = km * seconds;
     const m = parseInt(km * minutes) + parseInt(s / SECONDS_IN_MIN);
     const h = parseInt(m / SECONDS_IN_HOUR);
@@ -38,3 +43,23 @@ document.getElementsByName("btnTk")[0].addEventListener("click", () => {
 
     tkResult.textContent = time;
 });     
+
+document.getElementsByName("btnDk")[0].addEventListener("click", () => {
+    const dkMeters = document.getElementsByName("dkMeters")[0];
+    const kmHeures = document.getElementsByName("kmHeures")[0];
+    const dkResult = document.getElementById("dkResult");
+
+    const meter = dkMeters.value === "" ? 0 : parseInt(dkMeters.value);
+    const kmH = kmHeures.value === "" ? 0 : parseFloat(kmHeures.value);
+    const time = meter / METER_KM / kmH;
+    
+    const afp = getAfterFloatingPoint(time);
+    const h = parseInt(time);
+    const m = parseInt(afp * MINS_IN_HOUR);
+
+    dkResult.textContent = `${h} h ${m} mins`;
+});     
+
+function getAfterFloatingPoint(n) {
+    return n - Math.floor(n);
+}
